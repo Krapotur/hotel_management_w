9c10c41f841e335dcbdf679ba520d1b735aedead
Krapotur <68387294+Krapotur@users.noreply.github.com>
2024-03-19 14:56:09 +0300

    Preparing for deployment

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production'){
     app.get('*', (req,res) => {
         res.sendFile(
             path.resolve(
-                __dirname, 'client', 'dist', 'client', 'index.html'
+                __dirname, 'client', 'dist', 'client', 'browser', 'index.html'
             )
         )
     })
